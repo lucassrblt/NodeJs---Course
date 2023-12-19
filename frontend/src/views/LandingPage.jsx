2c7347fa188dc7291f1e71a5ac70aca87e5a3d97
Lucas RIMBAULT <lucas.rimbault@tentelemed.com>
2023-12-19 14:35:39 +0100

    Adding text animation landingpage

--- frontend/src/views/LandingPage.jsx
+++ frontend/src/views/LandingPage.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Terms from "../components/Terms";
 import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const navigate = useNavigate();
+  const circle = useRef();
+  const paragraphRef = useRef();
+
+  const txt = "Manage your social presence";
+  const baseline = [
+    "Foster community engagement and collaboration.",
+    "Establish a compelling online identity.",
+    "Enhance digital visibility and influence.",
+  ];
+
+  const textTypingEffect = (element, text, i = 0, lines = baseline) => {
+    // const length = text.length;
+    // let l = 0;
+
+    element.textContent = text.substring(0, i + 1);
+    if (i === text.length - 1) {
+      circle.current.classList.add("animated");
+      paragraphRef.current.classList.add("diseapear");
+      setTimeout(() => {
+        paragraphRef.current.classList.remove("diseapear");
+        circle.current.classList.remove("animated");
+        paragraphRef.current.textContent = "";
+
+        const nextLineIndex = (lines.indexOf(text) + 1) % lines.length;
+        textTypingEffect(paragraphRef.current, lines[nextLineIndex]);
+        // l++;
+        // textTypingEffect(mainText, baseline[1]);
+      }, 1500);
+      return;
+    }
+    setTimeout(() => {
+      textTypingEffect(element, text, i + 1);
+    }, 60);
+  };
+
+  useEffect(() => {
+    const mainText = paragraphRef.current;
+    textTypingEffect(mainText, baseline[0]);
+  }, []);
   return (
     <div className="view-landing">
       <div className="left-part">
         <div className="text">
           <div className="tall-txt">
-            <p>Manage your social presence</p>
+            <p className="tall-txt-p">Manage your social presence</p>
           </div>
           <div className="small-txt">
-            <p>to attract new customers</p>
+            <p ref={paragraphRef}></p>
+            <div className="hide-container" ref={circle}></div>
           </div>
         </div>
       </div>
